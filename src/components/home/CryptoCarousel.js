7b773f070f0e3c07a92826bd39aa5e8c9cc428fe
ukpono-j <ukponoakpan270@gmail.com>
2022-12-30 19:31:09 +0100

    first commit

--- src/components/home/CryptoCarousel.js
+++ src/components/home/CryptoCarousel.js
@@ -4,13 +4,13 @@ import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import Charts from '../charts/Charts';
-import {AiOutlineFall, AiOutlineRise } from 'react-icons/ai';
+import { AiOutlineFall, AiOutlineRise } from 'react-icons/ai';
 import { BsCurrencyBitcoin } from 'react-icons/bs';
-import {SiDash} from "react-icons/si"
-import {TbCurrencyLitecoin} from "react-icons/tb"
+import { SiDash } from "react-icons/si"
+import { TbCurrencyLitecoin } from "react-icons/tb"
 import CryptoCarouselFall from "../home/CryptoCarouselFall"
-import {FaEthereum} from "react-icons/fa"
-import {SiRipple} from "react-icons/si"
+import { FaEthereum } from "react-icons/fa"
+import { SiRipple } from "react-icons/si"
 
 
 const options = {
@@ -45,11 +45,11 @@ const CryptoCarousel = () => {
     return (
         <div className='crypto-container bg-[#010307] h-[430px] p-4 md:p-0'>
             <h1>Top Crypto Now</h1>
-            <OwlCarousel items="6" autoPlay dots className='owl-theme'  {...options}  margin={10} >
+            <OwlCarousel items="6" autoPlay dots className='owl-theme'  {...options} margin={10} >
                 <div className='item pl-3 pr-3 pt-6 pb-3'>
                     <div className='item-title h-auto flex items-center w-full'>
                         <div className='item-icon w-11 h-11 rounded-xl'>
-                            <SiDash/>
+                            <SiDash />
                         </div>
                         <div className='token-title pl-2'>
                             <h4>Dash<span>(Dash)</span></h4>
@@ -72,13 +72,13 @@ const CryptoCarousel = () => {
                         <Charts />
                     </div>
                     <div className='items-btn-container mt-10  w-full h-auto'>
-                        <button>Buy Coin</button>
+                        <button type='submit'>Buy Coin</button>
                     </div>
                 </div>
                 <div className='item pl-3 pr-3 pt-6 pb-3'>
                     <div className='item-title h-auto flex items-center w-full'>
                         <div className='item-icon w-11 h-11 rounded-xl'>
-                        <BsCurrencyBitcoin/>
+                            <BsCurrencyBitcoin />
                         </div>
                         <div className='token-title pl-2'>
                             <h4>Btc<span>(Bitcoin)</span></h4>
@@ -101,13 +101,13 @@ const CryptoCarousel = () => {
                         <CryptoCarouselFall />
                     </div>
                     <div className='items-btn-container mt-10  w-full h-auto'>
-                        <button>Buy Coin</button>
+                        <button type='submit'>Buy Coin</button>
                     </div>
                 </div>
                 <div className='item pl-3 pr-3 pt-6 pb-3'>
                     <div className='item-title h-auto flex items-center w-full'>
                         <div className='item-icon w-11 h-11 rounded-xl'>
-                            <TbCurrencyLitecoin/>
+                            <TbCurrencyLitecoin />
                         </div>
                         <div className='token-title pl-2'>
                             <h4>Lite<span>(Litecoin)</span></h4>
@@ -130,13 +130,13 @@ const CryptoCarousel = () => {
                         <Charts />
                     </div>
                     <div className='items-btn-container mt-10  w-full h-auto'>
-                        <button>Buy Coin</button>
+                        <button type='submit'>Buy Coin</button>
                     </div>
                 </div>
                 <div className='item pl-3 pr-3 pt-6 pb-3'>
                     <div className='item-title h-auto flex items-center w-full'>
                         <div className='item-icon w-11 h-11 rounded-xl'>
-                            <FaEthereum/>
+                            <FaEthereum />
                         </div>
                         <div className='token-title pl-2'>
                             <h4>eth<span>(ethereum)</span></h4>
@@ -159,13 +159,13 @@ const CryptoCarousel = () => {
                         <Charts />
                     </div>
                     <div className='items-btn-container mt-10  w-full h-auto'>
-                        <button>Buy Coin</button>
+                        <button type='submit'>Buy Coin</button>
                     </div>
                 </div>
                 <div className='item pl-3 pr-3 pt-6 pb-3'>
                     <div className='item-title h-auto flex items-center w-full'>
                         <div className='item-icon w-11 h-11 rounded-xl'>
-                            <SiRipple/>
+                            <SiRipple />
                         </div>
                         <div className='token-title pl-2'>
                             <h4>xrp<span>(Ripple)</span></h4>
@@ -188,13 +188,13 @@ const CryptoCarousel = () => {
                         <CryptoCarouselFall />
                     </div>
                     <div className='items-btn-container mt-10  w-full h-auto'>
-                        <button>Buy Coin</button>
+                        <button type='submit'>Buy Coin</button>
                     </div>
                 </div>
                 <div class='item pl-3 pr-3 pt-6 pb-3'>
                     <div className='item-title h-auto flex items-center w-full'>
                         <div className='item-icon w-11 h-11 rounded-xl'>
-                            <SiDash/>
+                            <SiDash />
                         </div>
                         <div className='token-title pl-2'>
                             <h4>Dash<span>(Dash)</span></h4>
@@ -217,7 +217,7 @@ const CryptoCarousel = () => {
                         <Charts />
                     </div>
                     <div className='items-btn-container mt-10  w-full h-auto'>
-                        <button>Buy Coin</button>
+                        <button type='submit'>Buy Coin</button>
                     </div>
                 </div>
 
