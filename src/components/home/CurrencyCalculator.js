3530f1b6f71b5ef6c9bea391555d0887796aaa97
ukpono-j <ukponoakpan270@gmail.com>
2022-12-31 11:39:48 +0100

    commit

--- src/components/home/CurrencyCalculator.js
+++ src/components/home/CurrencyCalculator.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CurrencyCalculator = () => {
   return (
-    <div className='h-auto w-full pl-[20px] md:pl-[80px] md:pr-[80px] bg-[#010307] max-w-[1420px] m-auto   pt-10 pb-14'>
+    <div className='h-auto w-full pl-[20px] md:pl-[80px] md:pr-[80px] pr-[20px] bg-[#010307] max-w-[1420px] m-auto   pt-10 pb-14'>
         <div className='border w-full h-[560px]'>
         CurrencyCalculator
         </div>
