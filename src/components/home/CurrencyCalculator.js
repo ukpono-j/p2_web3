627c27ad99a91dd354dd88f0af636bbcf4cf53db
ukpono-j <ukponoakpan270@gmail.com>
2022-12-31 12:47:07 +0100

    first commit

--- src/components/home/CurrencyCalculator.js
+++ src/components/home/CurrencyCalculator.js
@@ -7,7 +7,7 @@ const CurrencyCalculator = () => {
     return (
         <div className='h-auto w-full pl-[20px] md:pl-[80px] md:pr-[80px] pr-[20px] bg-[#010307] max-w-[1420px] m-auto   pt-10 pb-14'>
             <div className='border w-full h-[auto] grid sm:flex sm:item-start text-white justify-between'>
-                <div className='border current_bx max-w-[480px] h-[270px]'>
+                <div className='border current_bx max-w-[480px] h-[auto]'>
                     <h1>Bitcoin To Currency Calculator</h1>
                     <p>
                         Contact information: Include your name, email address, and phone number.
